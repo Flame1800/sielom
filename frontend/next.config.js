9f8382746734272ba89a704183e4c5f87cd9c0f8
Руслан Мухаметшафиков <r.shaficoff@SIELOM.NET>
2022-02-19 13:45:42 +0500

    edit src

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     env: {
         API_HOST_DEV: "http://localhost:1337/api",
         API_HOST_PROD: "https://new.sielom.ru/api",
-        API_URL: "https://new.sielom.ru/"
+        API_URL: "https://new.sielom.ru"
     }
 }
