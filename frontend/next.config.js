2df752e4a9453dec3039fc0296c1f0ee9aae707c
Руслан Мухаметшафиков <r.shaficoff@SIELOM.NET>
2022-02-19 13:41:36 +0500

    edit src

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
 
-const API_URL = process.env.NODE_ENV === 'development' ? "http://193.168.48.37:1337" : "http://193.168.48.37:1337"
 
 module.exports = {
     env: {
         API_HOST_DEV: "http://localhost:1337/api",
         API_HOST_PROD: "https://new.sielom.ru/api",
-        API_URL
+        API_URL: "https://new.sielom.ru/"
     }
 }
