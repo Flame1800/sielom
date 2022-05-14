9b294ff83f1bc233db6a7c87da6ca5f5c199325d
Vladimir <aquaform@yandex.ru>
2022-05-14 06:51:38 +0000

    fix config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     env: {
         API_HOST_DEV: "http://localhost:1337/api",
-        API_HOST_PROD: "https://new.sielom.ru/api",
-        API_URL: "https://new.sielom.ru"
+        API_HOST_PROD: "https://sielom.ru/api",
+        API_URL: "https://sielom.ru"
     }
 }
