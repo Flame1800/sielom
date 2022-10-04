e5a3045bd34b52d342f0acb87041dc88df457db7
flame1800 <ruslan08022001@gmail.com>
2022-10-04 18:26:28 +0500

    fix pages pub in path

--- frontend/pages/reception-campain/index.js
+++ frontend/pages/reception-campain/index.js
@@ -1,7 +1,5 @@
 import Layout from "../../components/Layouts/Layout";
 import { API } from "../../helpers/API";
-import Link from "next/link";
-import {} from "../../styles/homeStyle";
 import styled from "styled-components";
 import { baseTheme } from "../../styles/theme";
 import Head from "next/head";
@@ -26,40 +24,4 @@ ReceptionCampaignPage.getInitialProps = async () => {
   return { posts: posts.data.data, commission: commission.data.data };
 };
 
-const Wrapper = styled.div`
-  .commission {
-    border-bottom: 1px solid #3e3e3e;
-    margin-bottom: 100px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    margin-right: 50px;
-    margin-top: 90px;
-
-    .title {
-      margin-bottom: 30px;
-      font-weight: 600;
-      font-size: 26px;
-      color: ${baseTheme.colors.black};
-    }
-
-    .item {
-      margin-bottom: 40px;
-      max-width: 400px;
-
-      .name {
-        font-size: 18px;
-        font-weight: 600;
-        margin-bottom: 5px;
-        color: ${baseTheme.colors.light};
-      }
-    }
-  }
-
-  .pages {
-    max-width: 800px;
-    margin-bottom: 100px;
-  }
-`;
-
-export default ReceptionCampain;
+export default ReceptionCampaignPage;
