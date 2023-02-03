f94154eed20d609f57a8bd84e06b22f6492c5e6f
Vladimir <aquaform@yandex.ru>
2023-02-03 06:31:13 +0000

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,15 +3,15 @@ module.exports = {
     API_HOST_DEV: "http://localhost:1337/api",
     API_HOST_PROD: "https://sielom.ru/api",
     API_URL: "https://sielom.ru",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "sielom.ru",
-        port: "",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
+  }
+  //images: {
+  //  remotePatterns: [
+  //    {
+  //      protocol: "https",
+  //      hostname: "sielom.ru",
+  //      port: "",
+  //      pathname: "/uploads/**",
+  //    },
+  //  ],
+  //},
 };
